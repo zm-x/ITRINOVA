0c6fc53c2b551676ccac116f6ba0e44fe293e7db
zakaria el mountassire <zakariamountassire556@gmail.com>
2025-10-05 12:30:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1593,7 +1593,7 @@ const App = () => {
               
               {/* Learn More CTA */}
               <a 
-                href="#" 
+                href="/" 
                 className="inline-flex items-center px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition-colors duration-300"
               >
                 Learn More
@@ -1612,27 +1612,27 @@ const App = () => {
                     isBusiness ? 'text-black' : 'text-white'
                   }`}>Platform</h4>
                   <ul className="space-y-3">
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Talent Network</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>NASA Courses</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Enterprise Solutions</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>AI Matching Engine</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
@@ -1646,27 +1646,27 @@ const App = () => {
                     isBusiness ? 'text-black' : 'text-white'
                   }`}>Company</h4>
                   <ul className="space-y-3">
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>About Us</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Careers</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Contact</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Leadership Team</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
@@ -1680,27 +1680,27 @@ const App = () => {
                     isBusiness ? 'text-black' : 'text-white'
                   }`}>Compliance & Security</h4>
                   <ul className="space-y-3">
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Regulatory Compliance</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Responsible AI</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Privacy Policy</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Security Center</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
@@ -1714,27 +1714,27 @@ const App = () => {
                     isBusiness ? 'text-black' : 'text-white'
                   }`}>Support & Resources</h4>
                   <ul className="space-y-3">
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>FAQs</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Blog</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Help Center</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
                     }`}>Documentation</a></li>
-                    <li><a href="#" className={`transition-colors duration-300 ${
+                    <li><a href="/" className={`transition-colors duration-300 ${
                       isBusiness 
                         ? 'text-gray-600 hover:text-black' 
                         : 'text-gray-300 hover:text-white'
@@ -1752,7 +1752,7 @@ const App = () => {
             <div className="flex flex-col md:flex-row justify-between items-center">
               {/* Social Media Links */}
               <div className="flex space-x-6 mb-4 md:mb-0">
-                <a href="#" className={`transition-colors duration-300 ${
+                <a href="/" className={`transition-colors duration-300 ${
                   isBusiness 
                     ? 'text-gray-500 hover:text-black' 
                     : 'text-gray-400 hover:text-white'
@@ -1761,7 +1761,7 @@ const App = () => {
                     <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
                   </svg>
                 </a>
-                <a href="#" className={`transition-colors duration-300 ${
+                <a href="/" className={`transition-colors duration-300 ${
                   isBusiness 
                     ? 'text-gray-500 hover:text-black' 
                     : 'text-gray-400 hover:text-white'
@@ -1770,7 +1770,7 @@ const App = () => {
                     <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
                   </svg>
                 </a>
-                <a href="#" className={`transition-colors duration-300 ${
+                <a href="/" className={`transition-colors duration-300 ${
                   isBusiness 
                     ? 'text-gray-500 hover:text-black' 
                     : 'text-gray-400 hover:text-white'
